880be315241d4adc656e8ce71b8308c180248219
lahirunirmalx <lahirunirmalx@gamil.com>
2023-08-17 14:13:19 +0530

    add some data

--- data/LinksData.js
+++ data/LinksData.js
@@ -138,7 +138,7 @@ const webLinks = [
 
     {
         title: 'WhatsApp',
-        url: 'https://wa.me/+9475',
+        url: 'https://wa.me/+94756075965',
         type: 'other',
         icon: '/whatsapp.svg',
         on: true
